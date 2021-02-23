a9e8ff57faee65eeb9797183e674b6bc4288dd34
Adam-Almeida <adamportifolio@gmail.com>
2021-02-23 09:53:12 -0300

    Extrutura padrão do Next para qualquer projeto

---
+++ moveit-next/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { ExperienceBar } from '../Components/ExperienceBar';
+
+export default function Home() {
+  return (
+    <div className="container">
+      
+      <ExperienceBar />
+    </div>
+  )
+}
